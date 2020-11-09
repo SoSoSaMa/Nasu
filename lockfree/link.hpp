050c2889c2b38c55f2f694a7b5a3ba632e8c5cea
SoSoSaMa <leibingzhe2@gmail.com>
2020-11-09 20:19:54 +0800

    add a draft of link

--- lockfree/link.hpp
+++ lockfree/link.hpp
@@ -2,16 +2,19 @@
 
 #include <iostream>
 
+#include "pool.h"
+
 namespace Nasu::lockfree {
+template <typename T>
 class Link {
 public:
-    Link();
+    Link() = default;
 
-    void push_back();
+    ~Link() = default;
+
+    void push_back(T&& value);
 
     void erase();
-
-    
 
 private:
     uint32_t capacity;
